187e03969554f9de19011f75dfeba5597ae30707
Ulat-Dev <ulat_dev@outlook.com>
2022-06-20 15:35:04 +0800

    Added new topic descriptions.

--- catalog.py
+++ catalog.py
@@ -31,9 +31,36 @@ viewed, updated, and modified.
     Uses key-value pairs or a document storage model and is also 
     called NoSQL databases because they dont use SQL. Examples 
     are MongoDB and Redis.
-
+ 
 Web API
     This provides a set of rules for how the web applications' 
     data should be interacted with. They also function as an 
     intermediary between the user and the website/app.
+
+HTTP
+    It stands for Hypertext Transfer Protocol which allows web 
+    applications to communicate, retrieve resources, and exchange 
+    data. It works as a request-response protocol between a client 
+    and the server.
+* HTTP requests allows users to access content on the internet. 
+Once an HTTP request is made, the servers will respond by displaying 
+the resources to the user.
+
+TCP
+    It stands for Transmission Control Protocol. It is the intermediary 
+    between the client and server; in charge of disassembling the data 
+    into smaller parts before sending it over the network, and then 
+    reassembling it once it reaches its destination.
+IP 
+    An IP is in charge of the computer's communication, addresses, sends, 
+    and receives the data over the internet.
+
+* The GET method is used to request data from a specified resource.
+
+HTTPS (Secure)
+    It is an extension of HTTP that guarantees security; incharge of 
+    protecting data exchanged by encrypting it.
+
+* A web framework is a library of codes that simplify web development.
+Frameworks make it easier to reuse code.
 """
